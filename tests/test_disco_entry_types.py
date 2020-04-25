8841eaf9bad74fe55312ead83fef9045ec6e428f
dskrypa <dskrypa@gmail.com>
2020-04-25 13:28:55 -0400

    added some test cases

--- tests/test_disco_entry_types.py
+++ tests/test_disco_entry_types.py
@@ -4,11 +4,13 @@ import logging
 import sys
 from pathlib import Path
 from itertools import permutations
+from unittest.mock import MagicMock
 
 from ds_tools.test_common import main, TestCaseBase
 
 sys.path.append(Path(__file__).parents[1].joinpath('lib').as_posix())
 from music.common import DiscoEntryType
+from music.wiki.disco_entry import DiscoEntry
 
 log = logging.getLogger(__name__)
 
@@ -32,6 +34,22 @@ class DiscoEntryTypeTest(TestCaseBase):
     def test_most_specific_type(self):
         categories = {'mini albums', 'albums', '2020 mini albums', '2020 releases'}
         self.assertTypeResult(DiscoEntryType.MiniAlbum, categories)
+
+    def test_de_type_from_wiki_sections(self):
+        disco_entry = DiscoEntry(MagicMock(), MagicMock(), type_=['Extended plays'])
+        self.assertEqual(disco_entry.type, DiscoEntryType.ExtendedPlay)
+
+        disco_entry = DiscoEntry(MagicMock(), MagicMock(), type_=['Singles', 'As lead artist'])
+        self.assertEqual(disco_entry.type, DiscoEntryType.Single)
+
+        disco_entry = DiscoEntry(MagicMock(), MagicMock(), type_=['Studio albums'])
+        self.assertEqual(disco_entry.type, DiscoEntryType.Album)
+
+        disco_entry = DiscoEntry(MagicMock(), MagicMock(), type_=['Singles','As featured artist'])
+        self.assertEqual(disco_entry.type, DiscoEntryType.Feature)
+
+        disco_entry = DiscoEntry(MagicMock(), MagicMock(), type_=['Singles', 'Promotional singles'])
+        self.assertEqual(disco_entry.type, DiscoEntryType.Single)
 
 
 if __name__ == '__main__':

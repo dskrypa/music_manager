ca906c6462b1b0796d5e8f471d1c2c7c1299bad0
dskrypa <dskrypa@gmail.com>
2021-04-17 18:07:09 -0400

    started towards adding clean action as something that can be done via right-click

--- bin/gui_music_manager.py
+++ bin/gui_music_manager.py
@@ -3,8 +3,9 @@
 r"""
 How to add to right-click context menu - Windows:
     Computer\HKEY_CLASSES_ROOT\Directory\shell\Update Album Tags\command
-    C:\Users\dougs\git\ds_tools\venv\Scripts\python.exe "C:\Users\dougs\git\music_manager\bin\gui_music_manager.py" -a "%1"
+    C:\Users\dougs\git\ds_tools\venv\Scripts\python.exe "C:\Users\dougs\git\music_manager\bin\gui_music_manager.py" open "%1" -vv
 """
+# TODO: Add easy clean all / add bpm right-click action
 
 import sys
 from pathlib import Path
@@ -24,7 +25,10 @@ log = logging.getLogger(__name__)
 def parser():
     # fmt: off
     parser = ArgParser(description='Music Manager GUI')
-    parser.add_argument('--album_path', '-a', metavar='PATH', help='The path to an album to open rather than the main view')
+    with parser.add_subparser('action', 'open', help='Open directly to the Album view for the given path') as open_parser:
+        open_parser.add_argument('album_path', help='The path to the album to open')
+    with parser.add_subparser('action', 'clean', help='Open directly to the Clean view for the given path') as clean_parser:
+        clean_parser.add_argument('path', help='The directory containing files to clean')
     parser.include_common_args('verbosity')
     parser.add_common_sp_arg('--match_log', action='store_true', help='Enable debug logging for the album match processing logger')
     # fmt: on
@@ -53,13 +57,12 @@ def main():
     from PySimpleGUI import theme
     theme('SystemDefaultForReal')
 
-    start_kwargs = dict(
-        title='Music Manager',
-        resizable=True,
-        size=(1700, 750),
-        element_justification='center'
-    )
-    if args.album_path:
+    start_kwargs = dict(title='Music Manager', resizable=True, size=(1700, 750), element_justification='center')
+    if args.action == 'clean':
+        from music.gui.views.clean import CleanView
+        # TODO: Make CleanView support any path rather than an AlbumDir
+        # CleanView.start({'path': args.path}, **start_kwargs)
+    elif args.action == 'open':
         from music.gui.views.album import AlbumView
         from music.files.album import AlbumDir
         AlbumView.start({'album': AlbumDir(args.album_path)}, **start_kwargs)

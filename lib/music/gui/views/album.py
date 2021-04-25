740688665f7f6f7de9d6b8149d6ab8da5de73168
dskrypa <dskrypa@gmail.com>
2021-04-25 17:46:10 -0400

    fixed no rating selected handling

--- lib/music/gui/views/album.py
+++ lib/music/gui/views/album.py
@@ -182,11 +182,14 @@ class AlbumView(MainView, view_name='album'):
                         info_dict[field] = value
                     else:
                         # self.log.debug(f'Processing {key_type=} {obj=} {field=} {value=}')
-                        if field == 'rating' and value is not None:
-                            try:
-                                stars_to_256(int(value), 10)
-                            except ValueError as e:
-                                failed.append((data_key, f'Invalid rating for track={obj}:\n{e}'))
+                        if field == 'rating':
+                            if value:
+                                try:
+                                    stars_to_256(int(value), 10)
+                                except ValueError as e:
+                                    failed.append((data_key, f'Invalid rating for track={obj}:\n{e}'))
+                            else:
+                                continue
 
                         track_info_dict.setdefault(obj, {})[field] = value
 

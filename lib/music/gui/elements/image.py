4d8399c4435da930d74ad56eeda31e447852e76c
dskrypa <dskrypa@gmail.com>
2021-05-02 17:56:50 -0400

    fixed handling of incomplete image

--- lib/music/gui/elements/image.py
+++ lib/music/gui/elements/image.py
@@ -4,6 +4,7 @@ Image elements for PySimpleGUI
 :author: Doug Skrypa
 """
 
+import logging
 import math
 from io import BytesIO
 
@@ -14,6 +15,7 @@ from PySimpleGUI import Image
 from tkinter import Label
 
 __all__ = ['ExtendedImage']
+log = logging.getLogger(__name__)
 
 
 class ExtendedImage(Image):
@@ -28,6 +30,7 @@ class ExtendedImage(Image):
         self._background_color = background_color
         self._widget = None
         self._real_size = None
+        self._get_size(*self.Size)
 
     @property
     def Widget(self):
@@ -41,20 +44,28 @@ class ExtendedImage(Image):
         if self._image:
             self.resize(*self.Size)
 
-    def _resize(self, width: int, height: int):
+    def _get_size(self, width: int, height: int):
         image = self._image
         old_w, old_h = image.size
         new_w, new_h = calculate_resize(old_w, old_h, (width, height))
-        # self.log.log(19, f'Resizing image from {img_w}x{img_h} to {new_w}x{new_h}')
         self._real_size = (new_w, new_h)
-        return image.resize((new_w, new_h), 1)  # 1 = ANTIALIAS
+        return new_w, new_h
+
+    def _resize(self, width: int, height: int):
+        new_w, new_h = self._get_size(width, height)
+        # self.log.log(19, f'Resizing image from {img_w}x{img_h} to {new_w}x{new_h}')
+        return self._image.resize((new_w, new_h), 1)  # 1 = ANTIALIAS
 
     def resize(self, width: int, height: int):
         if self._image:
-            image = PhotoImage(self._resize(width, height))
-            self._widget.configure(image=image, width=width, height=height)
-            self._widget.image = image
-            self._widget.pack(padx=self.pad_used[0], pady=self.pad_used[1])
+            try:
+                image = PhotoImage(self._resize(width, height))
+            except OSError as e:
+                log.warning(f'Error resizing {self._image}: {e}')
+            else:
+                self._widget.configure(image=image, width=width, height=height)
+                self._widget.image = image
+                self._widget.pack(padx=self.pad_used[0], pady=self.pad_used[1])
 
 
 def calculate_resize(src_w, src_h, new_size):

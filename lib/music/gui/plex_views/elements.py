0d858377e30cc57f19f1b758d8d79a033d689ee4
dskrypa <dskrypa@gmail.com>
2021-05-23 11:36:53 -0400

    implemented ResultTable class; expanded result type handling to all expected types; initial view working but type changes currently result in incorrect layout

--- lib/music/gui/plex_views/elements.py
+++ lib/music/gui/plex_views/elements.py
@@ -7,117 +7,297 @@ High level PySimpleGUI elements that represent Plex objects
 import logging
 from datetime import datetime
 from itertools import count
+from math import ceil
+from operator import attrgetter
 from pathlib import Path
 from tempfile import gettempdir
+from typing import Union, Collection, Optional
 from urllib.parse import quote
 
-from plexapi.audio import Track  #, Audio, Album, Artist
+from plexapi.audio import Track, Album, Artist
 # from plexapi.playlist import Playlist
-# from plexapi.video import Video, Movie, Show, Season, Episode
-from PySimpleGUI import Column, Text
+from plexapi.video import Movie, Show, Season, Episode
+from PySimpleGUI import Column, HorizontalSeparator
 from requests import RequestException
 
 from ...common.images import as_image, scale_image, ImageType
+from ..constants import LoadingSpinner
 from ..elements import ExtendedImage, Rating, ExtText
+from ..popups.text import popup_ok
+from ..progress import Spinner
 
-__all__ = ['TrackRow']
+__all__ = ['ResultRow', 'ResultTable']
 log = logging.getLogger(__name__)
 ICONS_DIR = Path(__file__).resolve().parents[4].joinpath('icons')
 TMP_DIR = Path(gettempdir()).joinpath('plex', 'images')
+Result = Union[Track, Album, Artist, Movie, Show, Season, Episode]
+
+FIELD_SIZES = {
+    'year': (4, 1),
+    'artist': (30, 1),
+    'album': (30, 1),
+    'show': (30, 1),
+    'season': (20, 1),
+    'title': (30, 1),
+    'duration': (6, 1),
+    'plays': (5, 1),
+}
+TYPE_FIELDS_MAP = {
+    'track': {'cover', 'year', 'artist', 'album', 'title', 'duration', 'plays', 'rating'},
+    'album': {'cover', 'year', 'artist', 'title', 'plays', 'rating'},
+    'artist': {'image', 'title'},
+    'movie': {'image', 'year', 'title', 'duration', 'plays', 'rating'},
+    'show': {'image', 'year', 'title', 'duration', 'plays', 'rating'},
+    'season': {'image', 'year', 'show', 'title', 'plays'},
+    'episode': {'image', 'year', 'show', 'season', 'title', 'duration', 'plays', 'rating'},
+}
 
 
-class TrackRow:
+class ResultRow:
     __counter = count()
 
-    def __init__(self, cover_size: tuple[int, int] = None, with_column: bool = True):
-        self.cover_size = cover_size or (40, 40)
-        self._num = num = next(self.__counter)
-        kwargs = dict(visible=with_column)
-        self.cover = ExtendedImage(size=self.cover_size, key=f'track:{num}:cover', **kwargs)
-        self.year = Text(size=(4, 1), key=f'track:{num}:year', **kwargs)
-        self.artist = ExtText(size=(30, 1), key=f'track:{num}:artist', **kwargs)
-        self.album = ExtText(size=(30, 1), key=f'track:{num}:album', **kwargs)
-        self.title = Text(size=(30, 1), key=f'track:{num}:title', **kwargs)
-        self.duration = Text(size=(5, 1), key=f'track:{num}:duration', **kwargs)
-        self.plays = Text(size=(5, 1), key=f'track:{num}:views', justification='right', **kwargs)
-        self.rating = Rating(key=f'track:{num}:rating', **kwargs)
-        self.row = [self.cover, self.year, self.artist, self.album, self.title, self.duration, self.plays, self.rating]
-        if with_column:
-            self.column = Column(
-                [self.row],
-                key=f'track:{num}:column',
-                visible=False,
-                justification='center',
-                element_justification='center',
-                expand_x=True,
+    def __init__(self, result_type: str, img_size: tuple[int, int] = None, show_fields: Collection = None):
+        self.img_size = img_size or (40, 40)
+        self._num = next(self.__counter)
+        kp = f'result:{self._num}'
+        self.image = ExtendedImage(size=self.img_size, key=f'{kp}:cover')
+        self.result_type = result_type
+        show_fields = show_fields or TYPE_FIELDS_MAP[result_type]
+        self.fields: dict[str, ExtText] = {
+            field: ExtText(
+                size=size,
+                key=f'{kp}:{field}',
+                justification='right' if field == 'plays' else None,
+                visible=field in show_fields,
             )
-        else:
-            self.column = None
+            for field, size in FIELD_SIZES.items()
+        }
+        self.rating = Rating(key=f'{kp}:rating')
+        self.row = [self.image, *self.fields.values(), self.rating]
+        self.column = Column(
+            [self.row], key=kp, visible=False, justification='center', element_justification='center', expand_x=True
+        )
 
     def hide(self):
-        if self.column is None:
-            self.cover.update(visible=False)
-            self.rating.update(visible=False)
-        else:
-            self.column.update(visible=False)
+        self.column.update(visible=False)
 
     def clear(self, hide: bool = True):
         if hide:
             self.hide()
-        self.year.update('')
-        self.artist.update('')
-        self.album.update('')
-        self.title.update('')
-        self.duration.update('')
-        self.plays.update('')
-        self.cover.image = None
+        # visible = False if hide else None
+        for text_ele in self.fields.values():
+            text_ele.value = ''
+            # text_ele.update('')
+            # text_ele.update('', visible=visible)
+        self.image.image = None
         self.rating.update(0)
+        # self.rating.update(0, visible=visible)
 
-    def update(self, track: Track):
-        visible = None if self.column else True
-        self.year.update(track._data.attrib.get('parentYear'), visible=visible)
-        srv = track._server
-        base = f'{srv._baseurl}/web/index.html#!/server/{srv.machineIdentifier}/details?library%3Acontent.library'
-        self.artist.update(track.grandparentTitle, visible=visible, link=f'{base}&key={quote(track.grandparentKey)}')
-        self.album.update(track.parentTitle, visible=visible, link=f'{base}&key={quote(track.parentKey)}')
-        self.title.update(track.title, visible=visible)
-        duration = int(track.duration / 1000)
-        duration_dt = datetime.fromtimestamp(duration)
-        self.duration.update(duration_dt.strftime('%M:%S' if duration < 3600 else '%H:%M:%S'), visible=visible)
-        self.plays.update(track.viewCount, visible=visible)
-        self.rating.update(track.userRating, visible=visible)
-        self.cover.image = self._get_images(track)
-        if self.column is not None:
-            self.column.update(visible=True)
+    def set_result_type(self, result_type: str, show_fields: Collection = None, hide: bool = False):
+        if result_type != self.result_type:
+            self.result_type = result_type
+            show_fields = show_fields or TYPE_FIELDS_MAP[result_type]
+            for text_ele in self.fields.values():
+                text_ele.hide()
+            for field, text_ele in self.fields.items():
+                if field in show_fields:
+                    text_ele.make_visible()
+                # text_ele.update_visibility(field in show_fields)
+            if hide:
+                self.hide()
+            else:
+                self.column.expand(expand_x=True, expand_row=True)
 
-    def _get_images(self, track: Track):
-        full_size_path = TMP_DIR.joinpath(track.thumb[1:])
-        thumb_path = full_size_path.with_name('{}__{}x{}'.format(full_size_path.name, *self.cover_size))
-        if thumb_path.exists():
-            return thumb_path, full_size_path
-        elif full_size_path.exists():
-            return self._convert_and_save_thumbnail(full_size_path, thumb_path), full_size_path
+    def update(self, result: Result):
+        field_link_map = plex_links(result)
+        field_value_map = field_values(result)
+        # self.fields['title'].update(result.title, link=field_link_map.get('title'), visible=True)
+        self.fields['title'].update(result.title, link=field_link_map.get('title'))
+        for field, value in field_value_map.items():
+            # self.fields[field].update(value, link=field_link_map.get(field), visible=True)
+            self.fields[field].update(value, link=field_link_map.get(field))
+        if not isinstance(result, Season):
+            self.rating.update(result.userRating)
+        self.image.image = get_images(result, self.img_size)
+        self.column.update(visible=True)
 
-        server = track._server
+
+class ResultTable(Column):
+    __counter = count()
+
+    def __init__(
+        self, result_type: str, rows: int = 100, img_size: tuple[int, int] = None, sort_by: str = None, **kwargs
+    ):
+        show_fields = TYPE_FIELDS_MAP[result_type]
+        self.rows = [ResultRow(result_type, img_size, show_fields) for _ in range(rows)]
+        header_sizes = {'cover': (4, 1), 'image': (4, 1), **FIELD_SIZES, 'rating': (5, 1)}
+        self.headers: dict[str, ExtText] = {
+            header: ExtText(header.title(), size=size, key=f'header:{header}', visible=header in show_fields)
+            for header, size in header_sizes.items()
+        }
+        self.headers['year'].Pad = ((10, 5), 3)
+        self.header_column = Column([list(self.headers.values())], key='headers', justification='left')
+        self.result_type = result_type
+        self.results: Optional[list[Result]] = None
+        self.result_count = 0
+        self.last_page_count = 0
+        self.sort_by = sort_by or 'title'
+        self._num = next(self.__counter)
+        kwargs.setdefault('key', f'result_table:{self._num}')
+        kwargs.setdefault('scrollable', True)
+        kwargs.setdefault('vertical_scroll_only', True)
+        kwargs.setdefault('element_justification', 'center')
+        kwargs.setdefault('justification', 'center')
+        layout = [[self.header_column], [HorizontalSeparator()], *([tr.column] for tr in self.rows)]
+        super().__init__(layout, **kwargs)
+
+    def set_result_type(self, result_type: str):
+        if result_type != self.result_type:
+            self.result_type = result_type
+            show_fields = TYPE_FIELDS_MAP[result_type]
+            for header_ele in self.headers.values():
+                header_ele.hide()
+            for field, header_ele in self.headers.items():
+                if field in show_fields:
+                    header_ele.make_visible()
+                # header_ele.update_visibility(field in show_fields)
+            for row in self.rows:
+                if hide := row.column._visible:
+                    row.clear(False)
+                row.set_result_type(result_type, show_fields, hide)
+            self.header_column.expand(expand_x=True, expand_row=True)
+            self.expand(expand_x=True, expand_row=True)
+            self.contents_changed()
+
+    def show_results(self, results: set[Result], spinner: Spinner):
+        self.results = results = sorted(results, key=attrgetter(self.sort_by))
+        self.result_count = result_count = len(results)
+        pages = ceil(result_count / 100)
+        log.info(f'Found {result_count} ({pages=}) {self.result_type}s')
+        self.show_page(1, spinner)
+
+    def show_page(self, page: int, spinner: Spinner):
+        per_page = len(self.rows)
+        pages = ceil(self.result_count / per_page)
+        if pages < page or page <= 0:
+            return popup_ok(f'Invalid {page=} - must be a value between 1 and {pages}')
+
+        start = per_page * (page - 1)
+        end = start + per_page if page < pages else self.result_count
+        self.last_page_count = total = end - start
+        log.debug(f'Showing {page=} with {total}/{self.result_count} results ({start} - {end})')
+        for row, obj in spinner(zip(self.rows, self.results[start:end])):
+            row.update(obj)
+
+        if total < per_page:
+            for i in spinner(range(total, per_page)):
+                self.rows[i].clear()
+
         try:
-            resp = server._session.get(server.url(track.thumb), headers=server._headers())
-        except RequestException as e:
-            log.debug(f'Error retrieving cover for {track}: {e}')
-            return ICONS_DIR.joinpath('x.png')
-        else:
-            if not full_size_path.parent.exists():
-                full_size_path.parent.mkdir(parents=True)
-            log.debug(f'Saving cover image for {track} to {full_size_path.as_posix()}')
-            image_bytes = resp.content
-            with full_size_path.open('wb') as f:
-                f.write(image_bytes)
-            return self._convert_and_save_thumbnail(image_bytes, thumb_path), full_size_path
+            self.TKColFrame.canvas.yview_moveto(0)  # noqa
+        except Exception:
+            pass
+        self.expand(expand_y=True)  # including expand_x=True in this call results in no vertical scroll
+        self.expand(expand_x=True, expand_row=True)  # results in shorter y, but scroll works...
+        self.contents_changed()
 
-    def _convert_and_save_thumbnail(self, image: ImageType, thumb_path: Path):
-        thumbnail = scale_image(as_image(image), *self.cover_size)
-        if not thumb_path.parent.exists():
-            thumb_path.parent.mkdir(parents=True)
-        log.debug(f'Saving cover image thumbnail for to {thumb_path.as_posix()}')
-        with thumb_path.open('wb') as f:
-            thumbnail.save(f, 'png' if thumbnail.mode == 'RGBA' else 'jpeg')
-        return thumbnail
+    def sort_results(self, sort_by: str):
+        self.sort_by = sort_by
+        with Spinner(LoadingSpinner.blue_dots) as spinner:
+            self.results = sorted(self.results, key=attrgetter(self.sort_by))
+            self.show_page(1, spinner)
+
+    def clear_results(self):
+        self.results = None
+        self.result_count = 0
+        if self.last_page_count:
+            with Spinner(LoadingSpinner.blue_dots) as spinner:
+                for i in spinner(range(self.last_page_count)):
+                    self.rows[i].clear()
+
+        self.last_page_count = 0
+
+
+def get_images(result: Result, img_size: tuple[int, int]) -> Union[ImageType, tuple[ImageType, ImageType]]:
+    full_size_path = TMP_DIR.joinpath(result.thumb[1:])
+    thumb_path = full_size_path.with_name('{}__{}x{}'.format(full_size_path.name, *img_size))
+    if thumb_path.exists():
+        return thumb_path, full_size_path
+    elif full_size_path.exists():
+        return convert_and_save_thumbnail(full_size_path, thumb_path, img_size), full_size_path
+
+    server = result._server
+    try:
+        resp = server._session.get(server.url(result.thumb), headers=server._headers())
+    except RequestException as e:
+        log.debug(f'Error retrieving image for {result}: {e}')
+        return ICONS_DIR.joinpath('x.png')
+    else:
+        if not full_size_path.parent.exists():
+            full_size_path.parent.mkdir(parents=True)
+        log.debug(f'Saving image for {result} to {full_size_path.as_posix()}')
+        image_bytes = resp.content
+        with full_size_path.open('wb') as f:
+            f.write(image_bytes)
+        return convert_and_save_thumbnail(image_bytes, thumb_path, img_size), full_size_path
+
+
+def convert_and_save_thumbnail(image: ImageType, thumb_path: Path, img_size: tuple[int, int]):
+    thumbnail = scale_image(as_image(image), *img_size)
+    if not thumb_path.parent.exists():
+        thumb_path.parent.mkdir(parents=True)
+    log.debug(f'Saving image thumbnail to {thumb_path.as_posix()}')
+    with thumb_path.open('wb') as f:
+        thumbnail.save(f, 'png' if thumbnail.mode == 'RGBA' else 'jpeg')
+    return thumbnail
+
+
+def format_duration(duration_ms: int) -> str:
+    duration = int(duration_ms / 1000)
+    duration_dt = datetime.fromtimestamp(duration)
+    return duration_dt.strftime('%M:%S' if duration < 3600 else '%H:%M:%S')
+
+
+def field_values(result: Result) -> dict[str, str]:
+    if isinstance(result, Artist):
+        return {}
+
+    field_value_map = {'plays': result.viewCount}
+    if not isinstance(result, (Season, Album)):
+        field_value_map['duration'] = format_duration(result.duration)
+    if not isinstance(result, Track):
+        field_value_map['year'] = result.year
+
+    if isinstance(result, Track):
+        field_value_map.update(
+            artist=result.grandparentTitle, album=result.parentTitle, year=result._data.attrib.get('parentYear')
+        )
+    elif isinstance(result, Album):
+        field_value_map['artist'] = result.parentTitle
+    elif isinstance(result, Season):
+        field_value_map['show'] = result.parentTitle
+    elif isinstance(result, Episode):
+        field_value_map.update(show=result.grandparentTitle, season=result.parentTitle)
+
+    return field_value_map
+
+
+def plex_links(result: Result) -> dict[str, str]:
+    srv = result._server
+    base = f'{srv._baseurl}/web/index.html#!/server/{srv.machineIdentifier}/details?library%3Acontent.library&key='
+    if isinstance(result, Track):
+        return {'artist': f'{base}{quote(result.grandparentKey)}', 'album': f'{base}{quote(result.parentKey)}'}
+    elif isinstance(result, Album):
+        return {'artist': f'{base}{quote(result.parentKey)}', 'title': f'{base}{quote(result.key)}'}
+    elif isinstance(result, (Artist, Movie, Show)):
+        return {'title': f'{base}{quote(result.key)}'}
+    elif isinstance(result, Season):
+        return {'show': f'{base}{quote(result.parentKey)}', 'title': f'{base}{quote(result.key)}'}
+    elif isinstance(result, Episode):
+        return {
+            'show': f'{base}{quote(result.grandparentKey)}',
+            'season': f'{base}{quote(result.parentKey)}',
+            'title': f'{base}{quote(result.key)}',
+        }
+    else:
+        return {}
--- lib/music/gui/plex_views/search.py
+++ lib/music/gui/plex_views/search.py
@@ -4,29 +4,21 @@ View: Search results
 :author: Doug Skrypa
 """
 
-from math import ceil
-
-from PySimpleGUI import Text, Button, Combo, Column, Image
+from PySimpleGUI import Text, Button, Combo, Image
 
 from ...plex.query_parsing import PlexQuery, QueryParseError
 from ..base_view import event_handler, RenderArgs, Event, EventData
 from ..constants import LoadingSpinner
-from ..elements.inputs import ExtInput
+from ..elements import ExtInput
 from ..options import GuiOptions
 from ..popups.simple import popup_ok
 from ..popups.text import popup_error
 from ..progress import Spinner
-from .elements import TrackRow
+from .elements import ResultTable
+from .constants import LIB_TYPE_ENTITY_MAP
 from .main import PlexView
 
 __all__ = ['PlexSearchView']
-
-LIB_TYPE_ENTITY_MAP = {
-    'movie': ('Movie',),
-    'show': ('Show', 'Season', 'Episode'),
-    'artist': ('Track', 'Artist', 'Album'),
-    'photo': ('Album', 'Photo'),
-}
 
 
 class PlexSearchView(PlexView, view_name='search'):
@@ -37,35 +29,33 @@ class PlexSearchView(PlexView, view_name='search'):
             options.add_bool('allow_inst', 'Allow results that include instrumental versions of songs')
         with self.options.next_row() as options:
             options.add_input('escape', 'Escape regex special characters:', default='()')
-        self.track_rows = [TrackRow() for _ in range(100)]
-        win_w, win_h = self._window_size
-        headers = ('Cover', 'Year', 'Artist', 'Album', 'Title', 'Duration', 'Plays', 'Rating')
-        sizes = [(4, 1), (4, 1), (30, 1), (30, 1), (30, 1), (6, 1), (5, 1), (5, 1)]
-        # TODO: Make column widths dynamic
-        self.header_row = [Text(header, size=size, key=f'header:{header}') for header, size in zip(headers, sizes)]
-        self.header_row[1].Pad = ((10, 5), 3)
-        self.header_column = Column([self.header_row], key='headers', justification='left')
-        self.results = Column(
-            [[self.header_column], *([tr.column] for tr in self.track_rows)],
-            scrollable=True,
-            vertical_scroll_only=True,
-            key='results',
-            size=(900, win_h - 160),
-            element_justification='center',
-            justification='center',
+
+        self.lib_section = section = self.last_section
+        result_type = self.get_result_type(section)
+        self.section_picker = Combo(list(self.lib_sections), section.title, enable_events=True, key='section')
+        self.type_picker = Combo(
+            LIB_TYPE_ENTITY_MAP[section.type], result_type.title(), enable_events=True, key='entity_types'
         )
+        self.query = ExtInput('title~.*', size=(150, 1), key='query', focus=True)
+        self.results = ResultTable(result_type.lower(), size=(900, self._window_size[1] - 160))
+
+    @property
+    def result_type(self):
+        return self.results.result_type
+
+    @result_type.setter
+    def result_type(self, value: str):
+        self.results.set_result_type(value.lower())
 
     def get_render_args(self) -> RenderArgs:
         full_layout, kwargs = super().get_render_args()
 
-        last_section = self.last_section
-        entity_types = LIB_TYPE_ENTITY_MAP[last_section.type]
         search_row = [
             Text('Section:'),
-            Combo(list(self.lib_sections), last_section.title, enable_events=True, key='section'),
-            Combo(entity_types, entity_types[0], enable_events=True, key='entity_types'),
+            self.section_picker,
+            self.type_picker,
             Text('Query:'),
-            ExtInput('title~.*', size=(150, 1), key='query', focus=True),
+            self.query,
             Button('Search', bind_return_key=True),
         ]
         layout = [
@@ -79,32 +69,35 @@ class PlexSearchView(PlexView, view_name='search'):
 
     def post_render(self):
         super().post_render()
-        query = self.window.key_dict['query']
-        query.Widget.icursor(len(query.DefaultText))  # noqa
+        self.query.Widget.icursor(len(self.query.DefaultText))
 
-    def _output_size(self):
-        win_w, win_h = self._window_size
-        width, height = ((win_w - 180) // 7, (win_h - 214) // 16)
-        return width, height
+    # def _output_size(self):
+    #     win_w, win_h = self._window_size
+    #     width, height = ((win_w - 180) // 7, (win_h - 214) // 16)
+    #     return width, height
 
     @event_handler
     def section(self, event: Event, data: EventData):
-        window_ele_dict = self.window.key_dict
-        section_title = window_ele_dict['section'].get()  # noqa
         last_section = self.last_section
-        if section_title != last_section.title:
+        if (section_title := self.section_picker.get()) != last_section.title:
             self.config['lib_section'] = section_title
-            section = self.lib_sections[section_title]
+            self.lib_section = section = self.lib_sections[section_title]
             if section.type != last_section.type:
-                entity_types = LIB_TYPE_ENTITY_MAP[section.type]
-                window_ele_dict['entity_types'].update(entity_types[0], entity_types)  # noqa
+                self.result_type = result_type = self.get_result_type(section)
+                self.type_picker.update(result_type, LIB_TYPE_ENTITY_MAP[section.type])
+
+    @event_handler
+    def entity_types(self, event: Event, data: EventData):
+        if (result_type := self.type_picker.get().lower()) != self.result_type:
+            self.config[f'last_type:{self.lib_section.type}'] = self.result_type = result_type
 
     @event_handler
     def search(self, event: Event, data: EventData):
         self.options.parse(data)
-        window_ele_dict = self.window.key_dict
-        obj_type = window_ele_dict['entity_types'].get().lower()  # noqa
-        query = window_ele_dict['query'].value  # noqa
+        if (result_type := self.type_picker.get().lower()) != self.result_type:
+            self.config[f'last_type:{self.lib_section.type}'] = self.result_type = result_type
+
+        query = self.query.value
         try:
             kwargs = PlexQuery.parse(query, self.options['escape'], self.options['allow_inst'])
         except QueryParseError as e:
@@ -114,32 +107,13 @@ class PlexSearchView(PlexView, view_name='search'):
 
         with Spinner(LoadingSpinner.blue_dots) as spinner:
             spinner.update()
-            objects = self.plex.find_objects(obj_type, **kwargs)
+            objects = self.plex.find_objects(result_type, **kwargs)
             spinner.update()
             if not objects:
                 spinner.close()
                 return popup_ok('No results.')
 
-            obj_count = len(objects)
-            self.log.info(f'Found {obj_count} {obj_type}s for {query=}')
-            # pages = ceil(obj_count / 100)
-            # TODO: Pagination
-            # TODO: Sorting
-            # TODO: Make changing the rating actually change the rating
-            for row, obj in spinner(zip(self.track_rows, objects)):
-                row.update(obj)
-
-            if obj_count < 100:
-                for i in range(obj_count, 100):
-                    self.track_rows[i].clear()
-
-        try:
-            self.results.TKColFrame.canvas.yview_moveto(0)  # noqa
-        except Exception:
-            pass
-        self.results.expand(expand_y=True)  # including expand_x=True in this call results in no vertical scroll
-        self.results.expand(expand_x=True)  # results in shorter y, but scroll works...
-        self.results.contents_changed()
+            self.results.show_results(objects, spinner)
 
     @event_handler
     def window_resized(self, event: Event, data: EventData):

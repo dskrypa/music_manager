5e0d91755dc17bb6f3c5d1a47ba4c4ac31f895df
dskrypa <dskrypa@gmail.com>
2021-06-10 08:35:40 -0400

    fixed sigint handler resgistration to not occur when it shouldn't

--- lib/music/gui/window.py
+++ lib/music/gui/window.py
@@ -4,6 +4,7 @@ Extended Window class from PySimpleGUI
 :author: Doug Skrypa
 """
 
+import logging
 import signal
 from typing import Callable
 from weakref import WeakSet
@@ -11,12 +12,16 @@ from weakref import WeakSet
 from PySimpleGUI import Window as _Window
 
 __all__ = ['Window']
+log = logging.getLogger(__name__)
 
 
 class Window(_Window):
+    __registered_sigint_handler = None
     __instances = WeakSet()
 
     def __init__(self, *args, finalize_callback: Callable = None, **kwargs):
+        if self.__registered_sigint_handler is None:
+            self.register_sigint_handler()
         self._finalize_callback = finalize_callback
         super().__init__(*args, **kwargs)
         self.__instances.add(self)
@@ -36,7 +41,15 @@ class Window(_Window):
 
     @classmethod
     def unregister_sigint_handler(cls):
-        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        if cls.__registered_sigint_handler:
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
+        cls.__registered_sigint_handler = False
+
+    @classmethod
+    def register_sigint_handler(cls):
+        log.debug('Registering Window._handle_sigint to handle SIGINT')
+        signal.signal(signal.SIGINT, Window._handle_sigint)
+        cls.__registered_sigint_handler = True
 
     @classmethod
     def _handle_sigint(cls, *args):
@@ -49,6 +62,3 @@ class Window(_Window):
 
     def is_maximized(self) -> bool:
         return self.TKroot.state() == 'zoomed'
-
-
-signal.signal(signal.SIGINT, Window._handle_sigint)

dcb9726ab156557bc1576cfe1fc0fa2c525b293d
dskrypa <dskrypa@gmail.com>
2022-11-06 15:23:19 -0500

    added title_case func

--- lib/music/text/utils.py
+++ lib/music/text/utils.py
@@ -2,13 +2,14 @@
 :author: Doug Skrypa
 """
 
-from typing import Sequence, Iterable, Optional
+import re
+from typing import Sequence, Iterable, Optional, Match
 
 from ds_tools.utils.text_processing import common_suffix
 
 from .extraction import ends_with_enclosed, split_enclosed
 
-__all__ = ['combine_with_parens', 'find_ordinal']
+__all__ = ['combine_with_parens', 'find_ordinal', 'title_case']
 
 
 def combine_with_parens(parts: Iterable[str]) -> str:
@@ -47,3 +48,23 @@ def find_ordinal(text: str) -> Optional[int]:
     if m := ord_search(text.lower()):
         return ordinal_to_int[m.group(1)]
     return None
+
+
+CAPITALIZE_SUB = re.compile(r'\b\w+\b', re.IGNORECASE).sub
+
+
+def title_case(text: str):
+    num = 0
+
+    def _capitalize(match: Match) -> str:
+        group = match.group(0)
+        nonlocal num
+        num += 1
+        if num == 1:
+            return group[0].upper() + group[1:].lower()
+        lc_group = group.lower()
+        if lc_group in {'an', 'to', 'in', 'a', 'by', 'the', 'and', 'but', 'for', 'at', 'of'}:
+            return lc_group
+        return group[0].upper() + group[1:].lower()
+
+    return CAPITALIZE_SUB(_capitalize, text)

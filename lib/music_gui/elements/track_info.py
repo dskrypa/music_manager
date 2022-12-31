94d08b411103e0fcd14b418427c5c530d8981762
dskrypa <dskrypa@gmail.com>
2022-12-31 18:15:01 -0500

    started implementing view using SongFile objects

--- lib/music_gui/elements/track_info.py
+++ lib/music_gui/elements/track_info.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import logging
+from abc import ABC, abstractmethod
 from datetime import date
 from io import BytesIO
 from pathlib import Path
@@ -15,7 +16,7 @@ from requests import RequestException
 
 from ds_tools.caching.decorators import cached_property
 from ds_tools.fs.paths import get_user_cache_dir
-from tk_gui.elements import Element, Image, Text, Input, ListBox, Button
+from tk_gui.elements import Element, Image, Text, Input, ListBox, Button, HorizontalSeparator
 from tk_gui.elements.frame import InteractiveFrame, InteractiveRowFrame
 from tk_gui.elements.menu import Menu, MenuGroup
 from tk_gui.elements.menu.items import CopySelection, PasteClipboard, GoogleSelection, SearchKpopFandom, SearchGenerasia
@@ -31,12 +32,12 @@ from music.common.disco_entry import DiscoEntryType
 from music.common.ratings import stars_from_256
 from music.files.album import AlbumDir
 from music.files.exceptions import TagNotFound
+from music.files.track.track import SongFile
 from music.manager.update import AlbumInfo, TrackInfo
 
 if TYPE_CHECKING:
     from tkinter import Event
     from tk_gui.typing import Layout
-    from music.files.track.track import SongFile
 
 __all__ = []
 log = logging.getLogger(__name__)
@@ -138,25 +139,24 @@ class EditableListBox(InteractiveRowFrame):
         self.disabled = True
 
 
-class TrackInfoFrame(InteractiveFrame):
-    track_info: TrackInfo
-
-    def __init__(self, track: TrackInfo | SongFile, **kwargs):
-        if not isinstance(track, TrackInfo):
-            track = TrackInfo.from_file(track)
-        self.track_info = track
-        super().__init__(**kwargs)
+class TrackMixin:
+    disabled: bool
 
     def get_custom_layout(self) -> Layout:
-        return normalize_text_ele_widths([row for row in self.build_info_rows()])  # noqa
+        return normalize_text_ele_widths([row for row in self.build_rows()])  # noqa
 
-    @cached_property
+    def build_rows(self) -> Iterator[list[Element]]:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
     def path_str(self) -> str:
-        return self.track_info.path.as_posix()
+        raise NotImplementedError
 
-    @cached_property
+    @property
+    @abstractmethod
     def file_name(self) -> str:
-        return self.track_info.path.name
+        raise NotImplementedError
 
     def key_for(self, type: str, field: str, suffix: str = None) -> str:  # noqa
         if suffix:
@@ -169,6 +169,31 @@ class TrackInfoFrame(InteractiveFrame):
 
     def _key_text(self, key: str, suffix: str = None) -> Text:
         return Text(key.replace('_', ' ').title(), key=self.key_for('tag', key, suffix))
+
+    def _rating_row(self, key: str, value, suffix: str = None):
+        row = [
+            self._key_text(key, suffix),
+            Rating(value, key=self._val_key(key, suffix), show_value=True, pad=(0, 0), disabled=self.disabled),
+        ]
+        return row
+
+
+class TrackInfoFrame(TrackMixin, InteractiveFrame):
+    track_info: TrackInfo
+
+    def __init__(self, track: TrackInfo | SongFile, **kwargs):
+        if not isinstance(track, TrackInfo):
+            track = TrackInfo.from_file(track)
+        self.track_info = track
+        super().__init__(**kwargs)
+
+    @cached_property
+    def path_str(self) -> str:
+        return self.track_info.path.as_posix()
+
+    @cached_property
+    def file_name(self) -> str:
+        return self.track_info.path.name
 
     def build_info_rows(self, keys: Collection[str] = None) -> Iterator[list[Element]]:
         fields = ['artist', 'title', 'name', 'genre', 'disk', 'num']
@@ -194,11 +219,56 @@ class TrackInfoFrame(InteractiveFrame):
         if not keys or 'rating' in keys:
             yield self._rating_row('rating', data['rating'])
 
-    def _rating_row(self, key: str, value, suffix: str = None):
-        row = [
-            self._key_text(key, suffix),
-            Rating(value, key=self._val_key(key, suffix), show_value=True, pad=(0, 0), disabled=self.disabled),
+    def build_rows(self) -> Iterator[list[Element]]:
+        yield from self.build_info_rows()
+
+
+class SongFileFrame(TrackMixin, InteractiveFrame):
+    track: SongFile
+
+    def __init__(self, track: TrackInfo | SongFile | str | Path, **kwargs):
+        if isinstance(track, TrackInfo):
+            track = track.path
+        if isinstance(track, (str, Path)):
+            track = SongFile(track)
+        self.track = track
+        super().__init__(**kwargs)
+
+    @cached_property
+    def path_str(self) -> str:
+        return self.track.path.as_posix()
+
+    @cached_property
+    def file_name(self) -> str:
+        return self.track.path.name
+
+    def build_rows(self) -> Iterator[list[Element]]:
+        yield self.get_basic_info_row()
+        yield self.get_metadata_row()
+        # TODO: Tags
+
+    def get_basic_info_row(self):
+        track = self.track
+        # TODO: find way to show text with same appearance as disabled input instead
+        tag_version = f'{track.tag_version} (lossless)' if track.lossless else track.tag_version
+        return [
+            # Text('File:'), Text(self.file_name, size=(50, 1), path=self.path_str),
+            Text('File:'), Input(self.file_name, size=(50, 1), disabled=True),
+            Text('Length:'), Input(track.length_str, size=(6, 1), disabled=True),
+            Text('Type:'), Input(tag_version, size=(20, 1), disabled=True),
         ]
+
+    def get_metadata_row(self):
+        info = self.track.info
+        row = [
+            Text('Bitrate:'), Input(info['bitrate_str'], size=(14, 1), disabled=True),
+            Text('Sample Rate:'), Input(info['sample_rate_str'], size=(10, 1), disabled=True),
+            Text('Bit Depth:'), Input(info['bits_per_sample'], size=(10, 1), disabled=True),
+        ]
+        for key in ('encoder', 'codec'):
+            if value := info.get(key):
+                row.append(Text(f'{key.title()}:'))
+                row.append(Text(value, size=(15, 1)))
         return row
 
 
@@ -214,8 +284,34 @@ class TrackInfoView(View, title='Track Info'):
         self.album: AlbumInfo = album
 
     def get_init_layout(self) -> Layout:
-        return [[TrackInfoFrame(track)] for track in self.album.tracks.values()]
+        layout = []
+        for i, track in enumerate(self.album.tracks.values()):
+            if i:
+                layout.append([HorizontalSeparator()])
+            layout.append([TrackInfoFrame(track)])
+        return layout
+
+
+class SongFileView(View, title='Track Info'):
+    window_kwargs = {'exit_on_esc': True}
+
+    def __init__(self, album: AlbumInfo | AlbumDir | Path | str, **kwargs):
+        super().__init__(**kwargs)
+        if isinstance(album, AlbumInfo):
+            album = album.album_dir
+        elif isinstance(album, (Path, str)):
+            album = AlbumDir(album)
+        self.album: AlbumDir = album
+
+    def get_init_layout(self) -> Layout:
+        layout = []
+        for i, track in enumerate(self.album):
+            if i:
+                layout.append([HorizontalSeparator()])
+            layout.append([SongFileFrame(track)])
+        return layout
 
 
 if __name__ == '__main__':
-    TrackInfoView('C:/Users/dskry/etc/music_flac/kpop_2022-12-30/BIBI/BIBI [2019.05.15] BINU [FLAC-16bit-44.1kHz]').run()
+    # TrackInfoView('C:/Users/dskry/etc/music_flac/kpop_2022-12-30/BIBI/BIBI [2019.05.15] BINU [FLAC-16bit-44.1kHz]').run()
+    SongFileView('C:/Users/dskry/etc/music_flac/kpop_2022-12-30/BIBI/BIBI [2019.05.15] BINU [FLAC-16bit-44.1kHz]').run()

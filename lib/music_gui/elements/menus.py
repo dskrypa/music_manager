6bebf673ff18308e4f06731b813b712d1dc8d330
dskrypa <dskrypa@gmail.com>
2024-05-12 06:47:37 -0400

    added additional search site to right-click menu

--- lib/music_gui/elements/menus.py
+++ lib/music_gui/elements/menus.py
@@ -5,15 +5,19 @@ Right-click menus, menu bars, and menu items for the Music Manager GUI.
 from __future__ import annotations
 
 from tk_gui.elements.menu import Menu, MenuGroup, MenuItem, CloseWindow
-from tk_gui.elements.menu.items import CopySelection, PasteClipboard, OpenFileLocation, OpenFile, GoogleSelection
-from tk_gui.elements.menu.items import SearchKpopFandom, SearchGenerasia, SearchDramaWiki, SearchWikipedia
-from tk_gui.elements.menu.items import UpdateTextMenuItem, ToUpperCase, ToLowerCase
+from tk_gui.elements.menu.items import CopySelection, PasteClipboard, OpenFileLocation, OpenFile, SearchSelection
+from tk_gui.elements.menu.items import GoogleSelection as _Google, SearchWikipedia as _Wikipedia
+from tk_gui.elements.menu.items import SearchKpopFandom as _KpopFandom, SearchGenerasia as _Generasia
+from tk_gui.elements.menu.items import SearchDramaWiki as _DramaWiki, UpdateTextMenuItem, ToUpperCase, ToLowerCase
 from tk_gui.popups.about import AboutPopup
 
 from music.text.extraction import split_enclosed
 from music.text.utils import title_case
 
 __all__ = ['PathRightClickMenu', 'TextRightClickMenu', 'EditableTextRightClickMenu', 'MusicManagerMenuBar']
+
+
+# region Text Editing / Updating
 
 
 def flip_name_parts(text: str) -> str:
@@ -33,7 +37,13 @@ class ToTitleCase(UpdateTextMenuItem, update_func=title_case, label='Change case
     __slots__ = ()
 
 
+# endregion
+
 # region Right-Click Menus
+
+
+class _KindieFandom(SearchSelection, url='https://kindie.fandom.com/wiki/Special:Search?scope=internal&query={query}'):
+    __slots__ = ()
 
 
 class PathRightClickMenu(Menu):
@@ -42,14 +52,14 @@ class PathRightClickMenu(Menu):
         OpenFileLocation()
         OpenFile()
     with MenuGroup('Search'):
-        GoogleSelection(); SearchWikipedia(); SearchKpopFandom(); SearchDramaWiki(); SearchGenerasia()  # noqa
+        _Google(); _Wikipedia(); _KpopFandom(); _KindieFandom(); _DramaWiki(); _Generasia()  # noqa
 
 
 class TextRightClickMenu(Menu):
     CopySelection()
     PasteClipboard()
     with MenuGroup('Search'):
-        GoogleSelection(); SearchWikipedia(); SearchKpopFandom(); SearchDramaWiki(); SearchGenerasia()  # noqa
+        _Google(); _Wikipedia(); _KpopFandom(); _KindieFandom(); _DramaWiki(); _Generasia()  # noqa
 
 
 class EditableTextRightClickMenu(TextRightClickMenu):
@@ -64,7 +74,7 @@ class FullRightClickMenu(Menu):
         OpenFileLocation()
         OpenFile()
     with MenuGroup('Search'):
-        GoogleSelection(); SearchWikipedia(); SearchKpopFandom(); SearchDramaWiki(); SearchGenerasia()  # noqa
+        _Google(); _Wikipedia(); _KpopFandom(); _KindieFandom(); _DramaWiki(); _Generasia()  # noqa
     with MenuGroup('Update'):
         ToLowerCase(); ToUpperCase(); ToTitleCase(); FlipNameParts()  # noqa
 
